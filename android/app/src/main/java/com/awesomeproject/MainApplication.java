09ce78f411e16647e1b3b0a2f18d4c7ad9a800ab
chariss <charisschomba@gmail.com>
2019-07-31 17:52:54 +0300

    add image picker fun

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new MapsPackage(),
           new RNGestureHandlerPackage(),
           new VectorIconsPackage()
--- src/components/PickImage.js
+++ src/components/PickImage.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {View, Image, Button, StyleSheet} from 'react-native';
-import previewImage from "../assets/was.jpeg";
-
+import ImagePicker from 'react-native-image-picker';
 class PickImage extends React.Component {
+  state = {
+    pickedImage: null
+  };
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({title: 'Pick an image'}, res => {
+      if(res.didCancel) {
+        console.log('user cancelled')
+      } else if(res.error) {
+        console.log('Error', res.error)
+      } else {
+        this.setState({
+          pickedImage: {uri: res.uri}
+        })
+      }
+    })
+  };
   render(){
     return(
       <View style={styles.container}>
         <View style={styles.placeHolder}>
-          <Image source={previewImage} style={styles.previewImage}/>
+          <Image source={this.state.pickedImage} style={styles.previewImage}/>
         </View>
         <View style={styles.button}>
-          <Button title='Pick Image'/>
+          <Button title='Pick Image' onPress={this.pickImageHandler}/>
         </View>
       </View>
     );
